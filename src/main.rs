76e29e276ddba8ad48294042b2f77a119746783d
chitao1234 <chotaotao1qaz2wsx@gmail.com>
2024-02-12 15:53:26 +0800

    feat: add command line args

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,43 @@
-use std::convert::Infallible;
-use std::net::SocketAddr;
+use std::net::{Ipv6Addr, SocketAddr};
+use std::{convert::Infallible, net::IpAddr};
 
+use clap::Parser;
 use hyper::service::service_fn;
 use tokio::net::TcpListener;
-use tracing::{error, info};
+use tracing::{debug, error, info};
 
 use revproxy::revproxy;
 
+/// Run reverse proxy from normal proxy
+#[derive(Parser, Debug)]
+struct Cli {
+    /// The IP address to bind
+    #[arg(short, long, default_value_t = IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 0)))]
+    address: IpAddr,
+    /// The port to listen on
+    #[arg(short, long, default_value_t = 8001, value_parser = clap::value_parser!(u16).range(1..))]
+    port: u16,
+    /// Proxy host
+    #[arg(short = 'd', long, group = "proxy")]
+    proxy_host: Option<IpAddr>,
+    /// Proxy port
+    #[arg(short = 's', long, group = "proxy", value_parser = clap::value_parser!(u16).range(1..))]
+    proxy_port: Option<u16>,
+}
+
+// TODO: Proper use of span in tracing
 #[tokio::main]
 async fn main() -> Result<Infallible, anyhow::Error> {
     tracing_subscriber::fmt::init();
 
-    let addr: SocketAddr = "[::]:8001".parse()?;
+    let args = Cli::parse();
+    debug!("args: {:?}", args);
 
-    let listener = TcpListener::bind(addr).await?;
+    println!(
+        "Listening for connections on {}, port {}.",
+        args.address, args.port
+    );
+    let listener = TcpListener::bind(SocketAddr::new(args.address, args.port)).await?;
 
     loop {
         let (stream, _) = listener.accept().await?;
